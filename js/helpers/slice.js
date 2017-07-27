840b5d26467bb4093cdbeed871ccefe6b3e14b0e
Varun Hegde <varun@zenparent.in>
2017-07-27 15:48:09 +0530

    few style fixes

--- js/helpers/slice.js
+++ js/helpers/slice.js
@@ -1,6 +1,8 @@
 module.exports = function(context,start,end,options){
 	var template = '';
 
+	context  = context ? context : [];
+	
 	context = context.slice(start,end);
 
 	context.forEach(function(element){
